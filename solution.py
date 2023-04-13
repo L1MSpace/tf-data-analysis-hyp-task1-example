8270d8f14535fdee45c7f265676f2e8ade271496
L1MSpace <129736244+L1MSpace@users.noreply.github.com>
2023-04-13 16:12:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,5 @@
-import pandas as pd
-import numpy as np
-
+from math import sqrt
+from scipy.stats import norm
 
 chat_id = 704471350
 
@@ -8,24 +7,18 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    
+
     a1 = x_success
-    a2 = y_success
     b1 = x_cnt
+    a2 = y_success
     b2 = y_cnt
-    S = a1+a2+b1+b2
+    frac_a1b1 = a1 / b1
+    frac_a2b2 = a2 / b2
 
-    f_11 = ((a1+a2)*(a1+b1))/S
-    f_12 = ((a1+a2)*(a2+b2))/S
-    f_21 = ((b1+b2)*(a1+b1))/S
-    f_22 = ((b1+b2)*(a2+b2))/S
+    stan_error = sqrt( (frac_a1b1 * (1-frac_a1b1)/b1) + (frac_a2b2 * (1-frac_a2b2)/b2) )
 
-    chi2 = ( ((a1-f_11)**2)/f_11 ) + ( ((a2-f_12)**2)/f_12 ) + ( ((b1-f_21)**2)/f_21 ) + ( ((b2-f_22)**2)/f_22 )
-
-    chi2_crit = 5.412
-    if chi2 < chi2_crit:
-      res = False
-    else:
-      res = True
+    z_test = (frac_a2b2 - frac_a1b1) / stan_error
+    z_crit = norm.ppf(1 - 0.02)
+    res = z_test > z_crit
 
     return res
